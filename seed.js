a6da4d581e732ee6b64030ee5bf932064c06a159
Jatin Gupta <92937296+Jatin12a@users.noreply.github.com>
2024-07-20 17:37:52 +0530

    new chnges

--- seed.js
+++ seed.js
@@ -14,13 +14,8 @@ const dummyMovies = [
     director_name: "Frank Darabont",
     main_lead_name: "Tim Robbins, Morgan Freeman",
     genre: ["Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=6hB3S9bIaco",
+    video: "https://www.youtube.com/embed/6hB3S9bIaco",
     like: []
-=======
-    video:["../../datavideo/v1.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "The Godfather",
@@ -30,13 +25,8 @@ const dummyMovies = [
     director_name: "Francis Ford Coppola",
     main_lead_name: "Marlon Brando, Al Pacino, James Caan",
     genre: ["Crime", "Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=sY1S34973zA",
+    video: "https://www.youtube.com/embed/sY1S34973zA",
     like: []
-=======
-    video:["../../datavideo/v2.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "The Dark Knight",
@@ -46,13 +36,8 @@ const dummyMovies = [
     director_name: "Christopher Nolan",
     main_lead_name: "Christian Bale, Heath Ledger, Aaron Eckhart",
     genre: ["Action", "Crime", "Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+    video: "https://www.youtube.com/embed/EXeTwQWrcwY",
     like: []
-=======
-    video:["../../datavideo/v3.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Pulp Fiction",
@@ -62,13 +47,8 @@ const dummyMovies = [
     director_name: "Quentin Tarantino",
     main_lead_name: "John Travolta, Uma Thurman, Samuel L. Jackson",
     genre: ["Crime", "Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+    video: "https://www.youtube.com/embed/s7EdQ4FqbhY",
     like: []
-=======
-    video:["../../datavideo/v4.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Forrest Gump",
@@ -78,13 +58,8 @@ const dummyMovies = [
     director_name: "Robert Zemeckis",
     main_lead_name: "Tom Hanks, Robin Wright, Gary Sinise",
     genre: ["Drama", "Romance"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=bLvqoHBptjg",
+    video: "https://www.youtube.com/embed/bLvqoHBptjg",
     like: []
-=======
-    video:["../../datavideo/v5.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Inception",
@@ -94,135 +69,85 @@ const dummyMovies = [
     director_name: "Christopher Nolan",
     main_lead_name: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
     genre: ["Action", "Adventure", "Sci-Fi"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=YoHD9XEInc0",
+    video: "https://www.youtube.com/embed/YoHD9XEInc0",
     like: []
-=======
-    video:["../../datavideo/v6.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "The Matrix",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDLzY8P36UmIIcSPq1n4N3s9H6b1K5H2btYN5w8JQHw-COYDFjB2Z2Otlp8z6Os6Rhvgg&usqp=CAU",
+    poster: "https://example.com/posters/the_matrix.jpg",
     release_year: 1999,
     description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
     director_name: "Lana Wachowski, Lilly Wachowski",
     main_lead_name: "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
     genre: ["Action", "Sci-Fi"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=vKQi3bBA1y8",
+    video: "https://www.youtube.com/embed/vKQi3bBA1y8",
     like: []
-=======
-    video:["../../datavideo/v7.mp4","../../datavideo/v8.mp4"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Interstellar",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQipByyD9J78grL9dF4e81fXo0w1RVg19Dz6kR55_LzDWupA-hEdKNwA80KnG0gwPZa-Rk&usqp=CAU",
+    poster: "https://example.com/posters/interstellar.jpg",
     release_year: 2014,
     description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
     director_name: "Christopher Nolan",
     main_lead_name: "Matthew McConaughey, Anne Hathaway, Jessica Chastain",
     genre: ["Adventure", "Drama", "Sci-Fi"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+    video: "https://www.youtube.com/embed/zSWdZVtXT7E",
     like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Fight Club",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyF4o2mjFEpJxZs7F2hU3QslhXwK0lH2d70H0ZMtjdv_rzy8K4edkOL8sZGuW7e3J0lBk&usqp=CAU",
+    poster: "https://example.com/posters/fight_club.jpg",
     release_year: 1999,
     description: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
     director_name: "David Fincher",
     main_lead_name: "Brad Pitt, Edward Norton, Helena Bonham Carter",
     genre: ["Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=SUXWAEX2jlg",
+    video: "https://www.youtube.com/embed/SUXWAEX2jlg",
     like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "The Lord of the Rings: The Fellowship of the Ring",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8ghrZUN-KRQ4pH9l6M1O7H8g9j59Hv_4fquA7h1f9tRr7SwEADUV-3A2V_f0R4Z5tmV8A&usqp=CAU",
+    poster: "https://example.com/posters/lotr_fellowship.jpg",
     release_year: 2001,
     description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
     director_name: "Peter Jackson",
     main_lead_name: "Elijah Wood, Ian McKellen, Orlando Bloom",
     genre: ["Action", "Adventure", "Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=V75dMMIW2B4",
+    video: "https://www.youtube.com/embed/V75dMMIW2B4",
     like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "The Silence of the Lambs",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXQvZZtQzPpKNpdsE7_3G0pISaL0A8pFCVdOf_LPiSog3UUm9XY1qF2r8ZfHw4iw8GdM4Y&usqp=CAU",
+    poster: "https://example.com/posters/the_silence_of_the_lambs.jpg",
     release_year: 1991,
     description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
     director_name: "Jonathan Demme",
     main_lead_name: "Jodie Foster, Anthony Hopkins, Lawrence A. Bonney",
     genre: ["Crime", "Drama", "Thriller"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=W6Mm8Sbe__o",
+    video: "https://www.youtube.com/embed/W6Mm8Sbe__o",
     like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Schindler's List",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6lS1A1WGHtyib8Kv3RuWk0Gm8kQqgoW5pl1z4sFbOglVjTz2aIf2bJ1udvT-Y9uEdUg&usqp=CAU",
+    poster: "https://example.com/posters/schindlers_list.jpg",
     release_year: 1993,
     description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
     director_name: "Steven Spielberg",
     main_lead_name: "Liam Neeson, Ben Kingsley, Ralph Fiennes",
     genre: ["Biography", "Drama", "History"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=gG22XNhtnoY",
+    video: "https://www.youtube.com/embed/gG22XNhtnoY",
     like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
   },
   {
     title: "Gladiator",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6PAFNoDiJYj4YFZkVhRzqilK71PEFfiD3PzThqVcb0kXVF7xXINxKDFwiwDbiEek1YvA&usqp=CAU",
+    poster: "https://example.com/posters/gladiator.jpg",
     release_year: 2000,
     description: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
     director_name: "Ridley Scott",
     main_lead_name: "Russell Crowe, Joaquin Phoenix, Connie Nielsen",
     genre: ["Action", "Adventure", "Drama"],
-<<<<<<< HEAD
-    video: "https://www.youtube.com/watch?v=owK1bxDqXk0",
+    video: "https://www.youtube.com/embed/owK1qxDselE",
     like: []
-  },
-  {
-    title: "The Usual Suspects",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNLfORZjXzGxfC8xC_hhm08VgDzoKLRGyn0f6H_pMveKvHEDWZ4K5GdcO2wK2A8Z-iQl2Q&usqp=CAU",
-    release_year: 1995,
-    description: "A sole survivor tells of the twists and turns leading up to a horrific gun battle on a boat, which began when five con men met at a seemingly random gathering.",
-    director_name: "Bryan Singer",
-    main_lead_name: "Kevin Spacey, Gabriel Byrne, Chazz Palminteri",
-    genre: ["Crime", "Drama", "Mystery"],
-    video: "https://www.youtube.com/watch?v=oiXdPolca5o",
-    like: []
-=======
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
   },
   {
     title: "Titanic",
@@ -232,8 +157,8 @@ const dummyMovies = [
     director_name: "James Cameron",
     main_lead_name: "Leonardo DiCaprio, Kate Winslet, Billy Zane",
     genre: ["Drama", "Romance"],
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
+    video: "https://www.youtube.com/embed/kVrqfYjkTdQ",
+    like: []
   },
   {
     title: "The Departed",
@@ -243,11 +168,11 @@ const dummyMovies = [
     director_name: "Martin Scorsese",
     main_lead_name: "Leonardo DiCaprio, Matt Damon, Jack Nicholson",
     genre: ["Crime", "Drama", "Thriller"],
-    video:["https://youtu.be/4OGTQRcOFVA?si=yDc2jrActTqku1Yf"],
-    like:[]
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
+    video: "https://www.youtube.com/embed/auYbpnEwBBg",
+    like: []
   }
 ];
+
 
 
 
