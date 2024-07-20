6ab20c11dabbafcab1f26215b5b58400de11be8e
Jatin Gupta <92937296+Jatin12a@users.noreply.github.com>
2024-07-20 17:10:47 +0530

    Update movies.js

--- models/movies.js
+++ models/movies.js
@@ -31,11 +31,8 @@ const MovieSchema = new mongoose.Schema({
     }],
     video:{
         type:String
-<<<<<<< HEAD
+
     },
-=======
-    }],
->>>>>>> c4d3249bf13c731e83dba3c30c35a3e00f91135d
     like:[{
         type: mongoose.Schema.Types.ObjectId, ref:"user"}],
     comments:[{
